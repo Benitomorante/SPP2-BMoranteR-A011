aa89d6fe7a3f45f0d353cadb655c736ce30c345e
Benito Mv <Benito Mv@192.168.1.69>
2017-03-14 22:08:01 -0600

    BENITO MORANTE A01410590

---
+++ src/spp2/bmoranter/a11/SPP2BMoranteRA11.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp2.bmoranter.a11;
+
+/**
+ *
+ * @author Benito Mv
+ */
+public class SPP2BMoranteRA11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       window2 hi = new window2();
+       window3 hello = new window3();
+    }
+    
+}
---
+++ src/spp2/bmoranter/a11/window3.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp2.bmoranter.a11;
+import java.awt.*;
+/**
+ *
+ * @author Benito Mv
+ */
+public class window3 extends Frame{
+    //Atributos: 3 botones.
+    private final Button b1,b2,b3,b4,b5,b6,b7,b8,b9;
+    //Constructor
+    @SuppressWarnings("OverridableMethodCallInConstructor")
+    public window3 (){
+        super("Ventana 3");
+        setLayout(new GridLayout());
+        
+        b1 = new Button("1");
+        add (b1);
+        b2 = new Button("2");
+        add (b2);
+        b3 = new Button("3");
+        add (b3);
+        b4 = new Button("4");
+        add (b4);
+        b5 = new Button("5");
+        add (b5);
+        b6 = new Button("6");
+        add (b6);
+        b7 = new Button("7");
+        add (b7);
+        b8 = new Button("8");
+        add (b8);
+        b9 = new Button("9");
+        add (b9);
+        
+        
+        setSize(600,400);
+        setVisible(true);
+    
+    }
+}
